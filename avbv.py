659782fcc8691d328f8ccbc206f2a39496e26487
poly000 <34085039+poly000@users.noreply.github.com>
2024-02-02 16:52:15 +0800

    fix av2bv
    fix incorrect alphabet
    
    expand REV_TABLE

--- avbv.py
+++ avbv.py
@@ -1,39 +1,51 @@
-TABLE = ['f', 'Z', 'o', 'd', 'R', '9', 'X', 'Q', 'D', 'S', 'U', 'm', '2', '1', 'y', 'C', 'k', 'r', '6', 'z', 'B', 'q', 'i', 'v', 'e', 'Y', 'a', 'h', '8', 'b', 't', '4', 'x', 's', 'W', 'p', 'H', 'n', 'J', 'E', '7', 'j', 'L', '5', 'V', 'G', '3', 'g', 'u', 'M', 'T', 'K', 'N', 'P', 'A', 'w', 'c', 'F']
-REV_TABLE = { "F": 0, "c": 1, "w": 2, "A": 3, "P": 4, "N": 5, "K": 6, "T": 7, "M": 8, "u": 9, "g": 10, "3": 11, "G": 12, "V": 13, "5": 14, "L": 15, "j": 16, "7": 17, "E": 18, "J": 19, "n": 20, "H": 21, "p": 22, "W": 23, "s": 24, "x": 25, "4": 26, "t": 27, "b": 28, "8": 29, "h": 30, "a": 31, "Y": 32, "e": 33, "v": 34, "i": 35, "q": 36, "B": 37, "z": 38, "6": 39, "r": 40, "k": 41, "C": 42, "y": 43, "1": 44, "2": 45, "m": 46, "U": 47, "S": 48, "D": 49, "Q": 50, "X": 51, "9": 52, "R": 53, "d": 54, "o": 55, "Z": 56, "f": 57, }
+TABLE = [
+  'F', 'c', 'w', 'A', 'P', 'N', 'K', 'T', 'M', 'u', 'g', '3', 'G', 'V', '5', 'L',
+  'j', '7', 'E', 'J', 'n', 'H', 'p', 'W', 's', 'x', '4', 't', 'b', '8', 'h', 'a',
+  'Y', 'e', 'v', 'i', 'q', 'B', 'z', '6', 'r', 'k', 'C', 'y', '1', '2', 'm', 'U',
+  'S', 'D', 'Q', 'X', '9', 'R', 'd', 'o', 'Z', 'f',
+]
+REV_TABLE = { "F": 0, "c": 1, "w": 2, "A": 3, "P": 4, "N": 5, "K": 6, "T": 7,
+              "M": 8, "u": 9, "g": 10, "3": 11, "G": 12, "V": 13, "5": 14, "L": 15,
+              "j": 16, "7": 17, "E": 18, "J": 19, "n": 20, "H": 21, "p": 22, "W": 23,
+              "s": 24, "x": 25, "4": 26, "t": 27, "b": 28, "8": 29, "h": 30, "a": 31,
+              "Y": 32, "e": 33, "v": 34, "i": 35, "q": 36, "B": 37, "z": 38, "6": 39,
+              "r": 40, "k": 41, "C": 42, "y": 43, "1": 44, "2": 45, "m": 46, "U": 47,
+              "S": 48, "D": 49, "Q": 50, "X": 51, "9": 52, "R": 53, "d": 54, "o": 55,
+              "Z": 56, "f": 57, }
 
 MAX_AVID = 1 << 51
 MIN_AVID = 1
 
+XOR_CODE = 23442827791579
+MASK_CODE = 2251799813685247
+BASE = 58
+
 def av2bv(avid: str):
     if not avid.lower().startswith('av'):
         raise TypeError('invalid avid format')
-    avid = avid[2:]
-    avid = int(avid)
-    if avid <= MIN_AVID:
+    avid_num = int(avid[2:])
+    if avid_num < MIN_AVID:
         raise TypeError('avid too small')
-    if avid >= MAX_AVID:
+    if avid_num >= MAX_AVID:
         raise TypeError('avid too big')
-    c = 23442827791579
-    c2 = 2251799813685247
-    r = MAX_AVID | avid
-    r ^= c
-    y = []
-    for i in range(10):
-        p = pow(58, i)
-        p1 = r // p
-        y0 = p1 % 58
-        y.append(y0)
-    ans = []
-    for i in y:
-        ans.append(TABLE[i])
-    rand = [11, 10, 3, 8, 4, 6, 2, 9, 5, 7]
-    pair = list(zip(rand, ans))
-    pair.sort()
-    ans = ['B', 'V']
-    for z in pair:
-        ans.append(z[1])
-    ans = ''.join(ans)
-    return ans
+
+    r = MAX_AVID | avid_num
+    r ^= XOR_CODE
+    
+    ans = ['B', 'V', '1'] + ['0']*9
+    bvidx = len(ans) - 1
+
+    while r:
+        idx = r % BASE
+        code = TABLE[idx]
+        ans[bvidx] = code
+        r //= BASE
+        bvidx -= 1
+
+    swap(ans, 3, 9)
+    swap(ans, 4, 7)
+
+    return ''.join(ans)
 
 def bv2av(bvid: str):
     if not bvid.lower().startswith("bv1"):
@@ -48,11 +60,11 @@ def bv2av(bvid: str):
         index = REV_TABLE.get(byte, None)
         if index is None:
             raise TypeError('bvid contains illegal character')
-        avid *= 58
+        avid *= BASE
         avid += index
 
-    avid &= 2251799813685247
-    avid ^= 23442827791579
+    avid &= MASK_CODE
+    avid ^= XOR_CODE
     if avid < 0: raise TypeError('Illegal bvid')
     avid = str(avid)
     return 'av' + avid
